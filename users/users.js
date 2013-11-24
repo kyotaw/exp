0cd1e3342b95036cd9b4176e0aa19a51d979ab90
Kyota Watanabe <httg1326@gmail.com>
2013-11-25 01:48:40 +0900

    jquery mobile with angular

---
+++ users/users.js
@@ -0,0 +1,15 @@
+var users = 
+[
+	{
+		"name" : "watanabe kyota",
+		"mail" : "ssssss@sss.com"
+	},
+	{
+		"name" : "Not Registered",
+		"mail" : "Not Registered"
+	},
+	{
+		"name" : "Not Registered",
+		"mail" : "Not Registered"
+	}
+]
