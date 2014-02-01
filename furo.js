86f324638e453eafcbb80243b4ffcc30b36001ec
watanabe kyota <kyotaw@debian>
2014-02-01 22:46:36 +0900

    remove useless i/f

--- furo.js
+++
@@ -1,2 +0,0 @@
-//namespace object
-var FURO = {};
--- gamelib.js
+++
@@ -1,6 +0,0 @@
-document.write('<script type="text/javascript" src="lib/gamelib/furo.js"></script>')
-document.write('<script type="text/javascript" src="lib/gamelib/game.js"></scrupt>')
-document.write('<script type="text/javascript" src="lib/gamelib/framerate.js"></script>')
-document.write('<script type="text/javascript" src="lib/gamelib/graphics.js"></script>')
-document.write('<script type="text/javascript" src="lib/gamelib/axis.js"></script>')
-
