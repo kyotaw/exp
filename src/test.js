4e005526ba4f9faf40a5af462da3f922e9ff0591
watanabe kyota <kyotaw@debian>
2014-02-05 23:19:40 +0900

    keep highlight

--- src/test.js
+++ src/test.js
@@ -1,43 +1,20 @@
-var Monologue = (function(){
+Monologue = (function(){
+	_monologueCtrl = null;
 
+	function createLogueBox(evt){
+		_monologueCtrl = new MonologueController();
+		_monologueCtrl.show(evt);
+	}
 
 	return {
 		begin : function(){
-			$("body").mouseup(function(evt){
-				LogueBox.show(evt);	
-			});
-		}
-
-
-
-	}
-
-}());
-
-var LogueBox = (function(){
-	var showing = false;
-		
-	return {
-		show : function(evt){
-			if (showing) {
-				return;
-			}
-			var box = $("<div>")
-				.attr("id", "momo-box")
-				.css({"position":"absolute", "left":evt.pageX + "px", "top":evt.pageY + "px"})
-			var textarea = $("<textarea>")
-				.attr({"cols":20, "rows":5, "maxlength":200})
-				.css("resize", "none")
-			var button = $("<div>")
-				.addClass("paste-button")
-				.append($("<button>Paste</button>")
-					.css({"margin-top":"5px", "float":"right"})
-				);
-
-			box.append(textarea).append(button);
-			$("body").append(box);
-			textarea.focus();
-			showing = true;
+			$("body").bind("mouseup", createLogueBox);
+		},
+		end : function(){
+			$("body").unbind("mouseup", createLogueBox);
+			_monologueCtrl.hide();
+			_monologueCtrl = null;
 		}
 	}
+
 }());
